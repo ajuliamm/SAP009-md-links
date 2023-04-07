568a5ffabd4f78222ced4f09d09a84546428cc03
ajuliamm <ajuliammelo@gmail.com>
2023-04-07 10:25:26 -0300

    acessar links da função

--- index.js
+++ index.js
@@ -14,10 +14,15 @@
 
 const fs = require('fs')
 
-const readingFiles = () => {
-    fs.readFile('./folder/FILE.md', 'utf8', (err, data) => {
-        console.log(data)
+const mdLinks = (pathFile) => {
+    fs.readFile(pathFile, 'utf8', (err, data) => {
+
+        const regexLink = /\[\w+.\w+\]\(\w+.+\)/gmi;
+
+        const matchLinks = data.match(regexLink);
+
+        matchLinks.forEach(link => console.log(link))
     })
     
 }
-readingFiles()
+mdLinks('README.md')
