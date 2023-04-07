1dd4af752f4682a507f26780032298f8fb61858e
ajuliamm <ajuliammelo@gmail.com>
2023-04-07 10:12:59 -0300

    inicio da função

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-module.exports = () => {
-  // ...
-};
+// //este arquivo deve exportar a função mdLinks.
+// function init (){
+//  console.log('FUNÇÃO MD-LINKS')
+// };
+// module.exports = init;
+
+// function lona ( numero) {
+//     console.log(numero)
+//     if( numero > 0){
+//         lona(numero - 1)
+//     }
+// }
+// lona(10)
+
+const fs = require('fs')
+
+const readingFiles = () => {
+    fs.readFile('./folder/FILE.md', 'utf8', (err, data) => {
+        console.log(data)
+    })
+    
+}
+readingFiles()
