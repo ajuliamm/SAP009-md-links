ed69c93737f8b3511cf7409fbbc7434eba39dae1
ajuliamm <ajuliammelo@gmail.com>
2023-04-14 10:34:22 -0300

    verificação da flag validate

--- index.js
+++ index.js
@@ -3,48 +3,40 @@ const chalk = require('chalk');
 
 const mdLinks = (pathFile) => {
 
-    if(fs.statSync(pathFile).size === 0 || !fs.existsSync(pathFile)){
-        console.log(chalk.red('\u2717'), `O arquivo está vazio ou não existe`)
-        return;
-    }
+    return new Promise((resolve, reject) => {
 
-    fs.readFile(pathFile, 'utf8', (err, data) => {
+        if (fs.statSync(pathFile).size === 0 || !fs.existsSync(pathFile)) {
+            //console.log(chalk.red('\u2717'), `O arquivo está vazio ou não existe`)
+            //return;
+            reject(`${chalk.red('\u2717')} O arquivo está vazio ou não existe`)
+        } 
+        else {
 
-        const regexLink = /\[\w+.\w+\]\(\w+.+\)/gmi;
+            fs.readFile(pathFile, 'utf8', (abacate, data) => {
 
-        const matchLinks = data.match(regexLink);
+                const regexLink = /\[\w+.\w+\]\(\w+.+\)/gmi;
+                const matchLinks = data.match(regexLink);
+                const arrayLinks = matchLinks.map(link => {
 
-        matchLinks.forEach(link => {
-           
-            const removePunctuation = link.replace(')','').replace('[','')
-            const splitExpression = removePunctuation.split('](')
+                    const removePunctuation = link.replace(')', '').replace('[', '')
+                    const splitExpression = removePunctuation.split('](');
 
-            const objLinks = {
-                href: splitExpression[1],
-                text: splitExpression[0],
-                file: pathFile,
-            }
+                    const objLinks = {
+                        href: splitExpression[1],
+                        text: splitExpression[0],
+                        file: pathFile,
+                    }
+                    return objLinks;
 
-            accessHTTP(objLinks.href)
+                    //console.log(chalk.green('\u2714'), chalk.grey(objLinks.file), chalk.green(objLinks.href), chalk.grey(objLinks.text))
+                })
+                resolve(arrayLinks)
+                
+            })
+        }
 
-           console.log(chalk.green('\u2714'), chalk.grey(objLinks.file), chalk.green(objLinks.href), chalk.grey(objLinks.text))
-        })
     })
-    
 }
 
-const accessHTTP = (url) => {
-    fetch(url)
-    .then(response => {
-        console.log(url)
-        console.log(response.status)
-        console.log(response.ok)
-    })
-    .catch(err => {
-        console.error(err)
-        console.error(err.code)
-    })
-
-}
 
 module.exports = mdLinks;
